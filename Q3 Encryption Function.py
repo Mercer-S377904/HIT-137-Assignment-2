c90a7f1007ee43b2447ffe6d73996fc356e46e96
Mercer-S377904 <s377904@students.cdu.edu.au>
2024-09-04 23:34:54 +1000

    Update Q3 Encryption Function.py

--- Q3 Encryption Function.py
+++ Q3 Encryption Function.py
@@ -1,9 +1,9 @@
 def encrypt(text, key):
     encrypted_text = ""
     for char in text:
-        if char.isalpha(): #shifts letters only
+        if char.isalpha(): #Shifts letters only
             shifted = ord(char) + key #shift adds key only
-            # Line 7 to 16 ensures letters dont get shifted into charachters by shifting from z back to a
+            #Line 7 to 16 ensures letters dont get shifted into charachters by shifting from z back to a
             if char.islower(): 
                 if shifted > ord('z'):
                     shifted -= 26
