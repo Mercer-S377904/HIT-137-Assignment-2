1db66a9647b7d2f8b5c0bf89cefcf6c589f39cb3
Mercer-S377904 <s377904@students.cdu.edu.au>
2024-09-04 23:37:33 +1000

    Update Q3 Key Code.py

--- Q3 Key Code.py
+++ Q3 Key Code.py
@@ -1,21 +1,21 @@
-    total = 0
-    for i in range(5):
-        for j in range(3):
-            if i + j == 5:
-                total += i + j
-            else:
-                total -= i - j
-    
-    counter = 0
-    while counter < 5:
-        #line 12 to 15 ensures the total is equal to 13
-        if total < 13:
-            total += 1
-        elif total > 13:
-            total -= 1
-        #If total is not less than or more than 13, increase counter (basically checks 3 times to ensure 13)
+total = 0
+for i in range(5):
+    for j in range(3):
+        if i + j == 5:
+            total += i + j
         else:
-            counter += 2
+            total -= i - j
+    
+counter = 0
+while counter < 5:
+    #line 12 to 15 ensures the total is equal to 13
+    if total < 13:
+        total += 1
+    elif total > 13:
+        total -= 1
+    #If total is not less than or more than 13, increase counter (basically checks 3 times to ensure 13)
+    else:
+        counter += 2
 
 
 #If lines 12 to 15 force total to equal 13 why not just use total=13 or rather key=13?
