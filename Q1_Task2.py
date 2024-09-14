d82ffcf0cb48a42fc39023f848b9de405c4899c7
Mercer-S377904 <s377904@students.cdu.edu.au>
2024-09-14 17:00:42 +1000

    Q1 Installs.py

--- Q1_Task2.py
+++ Q1_Task2.py
@@ -1,7 +1,8 @@
 # Question 1 Task 2
 # The commands through powershell to install the required libraries
 
-! pip install spacy
-! pip install scispacy
-! pip install transformers
-! pip install pip install https://s3-us-west-2.amazonaws.com/ai2-s2-scispacy/releases/v0.5.4/en_core_sci_sm-0.5.4.tar.gz
+pip install pandas
+pip install spacy
+pip install scispacy
+pip install transformers
+pip install pip install https://s3-us-west-2.amazonaws.com/ai2-s2-scispacy/releases/v0.5.4/en_core_sci_sm-0.5.4.tar.gz
