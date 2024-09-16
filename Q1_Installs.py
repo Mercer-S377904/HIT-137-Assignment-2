f17b1f7e4cdaeed731f3885b0e22b91712fdcda4
Bailey-Holloway <S376066@students.cdu.edu.au>
2024-09-16 10:20:39 +1000

    Update Q1_Installs.py

--- Q1_Installs.py
+++ Q1_Installs.py
@@ -1,8 +1,9 @@
-# Question 1
+# Question 1 Task 2
 # The commands through powershell to install the required libraries
 
-pip install pandas
-pip install spacy
-pip install scispacy
-pip install transformers
-pip install pip install https://s3-us-west-2.amazonaws.com/ai2-s2-scispacy/releases/v0.5.4/en_core_sci_sm-0.5.4.tar.gz
+! pip install nltk
+! pip install spacy
+! pip install scispacy
+! pip install transformers
+! pip install https://s3-us-west-2.amazonaws.com/ai2-s2-scispacy/releases/v0.5.4/en_core_sci_sm-0.5.4.tar.gz
+! pip install https://s3-us-west-2.amazonaws.com/allenai-models/en_ner_bc5cdr_md-0.4.0.tar.gz
