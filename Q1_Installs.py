bcf7f71a6bc1476457aaa8619cd43eda475fdbfe
CraigsGits <163123402+CraigsGits@users.noreply.github.com>
2024-09-17 22:13:21 +1000

    Update Q1_Installs.py

--- Q1_Installs.py
+++ Q1_Installs.py
@@ -6,4 +6,4 @@
 ! pip install scispacy
 ! pip install transformers
 ! pip install https://s3-us-west-2.amazonaws.com/ai2-s2-scispacy/releases/v0.5.4/en_core_sci_sm-0.5.4.tar.gz
-! pip install https://s3-us-west-2.amazonaws.com/allenai-models/en_ner_bc5cdr_md-0.4.0.tar.gz
+! pip install https://s3-us-west-2.amazonaws.com/ai2-s2-scispacy/releases/v0.5.4/en_ner_bc5cdr_md-0.5.4.tar.gz
